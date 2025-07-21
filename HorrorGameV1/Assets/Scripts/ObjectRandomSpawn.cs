8b99aba11678fb34074572c24ecc8e17bfe751dc
Berkay <61667359+BerkayDemiral@users.noreply.github.com>
2025-07-21 19:00:52 +0300

    feat: Add random spawn system for objects

---
+++ HorrorGameV1/Assets/Scripts/ObjectRandomSpawn.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRandomSpawn : MonoBehaviour
+{
+    public Transform oB;
+
+    public Transform[] spawnPoints;
+
+    void Start()
+    {
+        int indexNumber = Random.Range(0, spawnPoints.Length);
+        oB.position = spawnPoints[indexNumber].position;
+    }
+
+    private void Update()
+    {
+
+    }
+}
